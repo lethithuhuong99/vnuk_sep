1bf8f05ab5124d8196feffc74361b1ee3a3f0de0
lethithuhuong99 <huong.le170205@vnuk.edu.vn>
2019-05-13 11:24:22 +0700

    a

---
+++ src/vn/edu/vnuk/sep/EnterNewMinimunWage.java
@@ -0,0 +1,69 @@
+package vn.edu.vnuk.sep;
+
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+
+@SuppressWarnings("serial")
+public class EnterNewMinimunWage extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					EnterNewMinimunWage frame = new EnterNewMinimunWage();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public EnterNewMinimunWage() {
+		setTitle("Add Minimum Wage");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblMinimunWage = new JLabel("Minimun Wage:");
+		lblMinimunWage.setBounds(83, 88, 90, 14);
+		contentPane.add(lblMinimunWage);
+		
+		textField = new JTextField();
+		textField.setBounds(192, 85, 139, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JButton btnNewButton = new JButton("Add");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnNewButton.setBounds(192, 146, 64, 23);
+		contentPane.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Cancel");
+		btnNewButton_1.setBounds(266, 146, 65, 23);
+		contentPane.add(btnNewButton_1);
+	}
+}
